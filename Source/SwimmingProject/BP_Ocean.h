080c00336003ec6291c8ae5e4251dace48a5cc07
rr-10 <remorejithomas@gmail.com>
2022-07-07 02:39:09 +0100

    Reorganisation of project
    Oh dear...

---
+++ Source/SwimmingProject/BP_Ocean.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WaterBodyOceanActor.h"
+#include "BP_Ocean.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWIMMINGPROJECT_API ABP_Ocean : public AWaterBodyOcean
+{
+	GENERATED_BODY()
+	
+};
